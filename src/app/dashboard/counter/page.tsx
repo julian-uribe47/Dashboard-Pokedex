02f933717dbce41b05621b1c34dc55ee7fcb7e17
unknown <juribedio1221@gmail.com>
2024-03-09 15:06:37 -0500

    Fin sección de page counter

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,19 @@
 
+import { CartCounter } from "@/shopping-cart";
+
+
+export const metadata = {
+  title: 'Shopping-cart',
+  description: 'Contador',
+};
+
 export default function CounterPage() {
+
   return (
-    <div>
-      <h1> Counter Page</h1>
+    <div className=" flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+
+      <CartCounter value={47} />
     </div>
   );
 }
