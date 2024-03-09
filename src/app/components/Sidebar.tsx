d311e8ad6664a07ff3ee3bc00f42fe8d1c8e14c6
unknown <juribedio1221@gmail.com>
2024-03-09 18:32:06 -0500

    inicio página pokedex

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,28 +1,34 @@
 import Image from "next/image"
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from "react-icons/io5"
+import { IoBrowsersOutline, IoCalculator, IoLogoOctocat, IoLogoReact } from "react-icons/io5"
 import { SidebarMenuItem } from "./SidebarMenuItem"
 import path from "path"
 
 const menuItems = [
     {
         path: '/dashboard/main',
-        icon: <IoBrowsersOutline size={40} />,
+        icon: <IoBrowsersOutline size={30} />,
         title: 'Dashboard',
         subtitle: 'Visualización'
     },
     {
         path: '/dashboard/counter',
-        icon: <IoCalculator size={40} />,
+        icon: <IoCalculator size={30} />,
         title: 'Counter',
         subtitle: 'Contador Client Side'
     },
+    {
+        path: '/dashboard/pokemons',
+        icon: <IoLogoOctocat size={30} />,
+        title: 'Pokedex',
+        subtitle: 'Generación estática'
+    }
 ]
 
 export const Sidebar = () => {
     return (
         <div id="menu"
             style={{ width: '400px' }}
-            className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+            className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll">
 
             <div id="logo" className="my-4 px-6">
                 <h1 className="text-lg md:text-2xl font-bold text-white flex items-center">
@@ -39,7 +45,8 @@ export const Sidebar = () => {
                             src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c"
                             alt="user avatar"
                             width={50}
-                            height={50} />
+                            height={50}
+                            priority/>
                     </span>
                     <span className="text-sm md:text-base font-bold">
                         Julian Uribe
---
+++ src/app/pokemons/index.ts
@@ -0,0 +1,4 @@
+export { PokemonCard } from './components/PokemonCard';
+export {PokemonGrid} from './components/PokemonGrid';
+export type {PokemonsResponse} from './interfaces/pokemons-response';
+export type {SimplePokemon} from './interfaces/simple-pokemon';
